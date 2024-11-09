e681df312c57beac5852c64790ca0a9e935eb9e7
TLRKiliann <philogenie@protonmail.com>
2024-11-09 14:39:35 +0100

    foundations of the game done

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,34 +1,224 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function App(): JSX.Element {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='frame'>
+
+      <div className='top-frame'>
+        <div className='squares square-top'>
+          <p>0</p>
+        </div>
+        <div className='squares square-top'>
+          <p>55</p>
+        </div>
+        <div className='squares square-top'>
+          <p>54</p>
+        </div>
+        <div className='squares square-top'>
+          <p>53</p>
+        </div>
+        <div className='squares square-top'>
+          <p>52</p>
+          <h4>Quiz</h4>
+        </div>
+        <div className='squares square-top'>
+          <p>51</p>
+        </div>
+        <div className='squares square-top'>
+          <p>50</p>
+        </div>
+        <div className='squares square-top'>
+          <p>49</p>
+        </div>
+        <div className='squares square-top'>
+          <p>48</p>
+          <h4>Sanction</h4> 
+        </div>
+        <div className='squares square-top'>
+          <p>47</p>
+        </div>
+        <div className='squares square-top'>
+          <p>46</p>
+        </div>
+        <div className='squares square-top'>
+          <p>45</p>
+        </div>
+        <div className='squares square-top'>
+          <p>44</p>
+          <h4>Bonne Action</h4>
+        </div>
+        <div className='squares square-top'>
+          <p>43</p>
+        </div>
+        <div className='squares square-top'>
+          <p>42</p>
+        </div>
+        <div className='squares square-top'>
+          <p>41</p>
+        </div>
+        <div className='squares square-top'>
+          <p>40</p>
+          <h4>Defi</h4>
+        </div>
+        <div className='squares square-top'>
+          <p>39</p>
+        </div>      
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+
+      <div className="middle-frames">
+
+        <div className='left-frame'>
+          <div className='squares-side squares-lside'>
+            <p>1</p>
+          </div>
+          <div className='squares-side squares-lside'>
+            <p>2</p>
+          </div>
+          <div className='squares-side squares-lside'>
+            <p>3</p>
+          </div>
+          <div className='squares-side squares-lside'>
+            <p>4</p>
+            <h4>Quiz</h4>
+          </div>
+          <div className='squares-side squares-lside'>
+            <p>5</p>
+          </div>
+          <div className='squares-side squares-lside'>
+            <p>6</p>
+          </div>
+          <div className='squares-side squares-lside'>
+            <p>7</p>
+          </div>
+          <div className='squares-side squares-lside'>
+            <p>8</p>
+            <h4>Defi</h4>
+          </div>
+          <div className='squares-side squares-lside'>
+            <p>9</p>
+          </div>
+          <div className='squares-side squares-lside'>
+            <p>10</p>
+          </div>
+          <div className='squares-side squares-lside'>
+            <p>11</p>
+          </div>
+          <div className='squares-side squares-lside'>
+            <p>12</p>
+          </div>
+        </div>
+
+        <div className="container-cards">
+          <div className='cards-box cards-box-left'>
+            <div className="card">Defis</div>
+            <div className="card">Quiz</div>
+          </div>
+          <div className='cards-box cards-box-right'>
+            <div className="card">Sanctions</div>
+            <div className="card">Bonnes Actions</div>
+          </div>
+        </div>
+
+        <div className='right-frame'>
+          <div className='squares-side squares-rside'>
+            <p>38</p>
+          </div>
+          <div className='squares-side squares-rside'>
+            <p>37</p>
+            <h4>Quiz</h4>
+          </div>
+          <div className='squares-side squares-rside'>
+            <p>36</p>
+          </div>
+          <div className='squares-side squares-rside'>
+            <p>35</p>
+          </div>
+          <div className='squares-side squares-rside'>
+            <p>34</p>
+          </div>
+          <div className='squares-side squares-rside'>
+            <p>33</p>
+            <h4>Defi</h4>
+          </div>
+          <div className='squares-side squares-rside'>
+            <p>32</p>
+          </div>
+          <div className='squares-side squares-rside'>
+            <p>31</p>
+          </div>
+          <div className='squares-side squares-rside'>
+            <p>30</p>
+          </div>
+          <div className='squares-side squares-rside'>
+            <p>29</p>
+          </div>
+        </div>
+
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+      <div className='bottom-frame'>
+        <div className="squares square-bottom">
+          <p>11</p>
+        </div>
+        <div className="squares square-bottom">
+          <p>12</p>
+          <h4>Bonne Action</h4>
+        </div>
+        <div className="squares square-bottom">
+          <p>13</p>
+        </div>
+        <div className="squares square-bottom">
+          <p>14</p>
+        </div>
+        <div className="squares square-bottom">
+          <p>15</p>
+        </div>
+        <div className="squares square-bottom">
+          <p>16</p>
+          <h4>Sanction</h4>
+        </div>
+        <div className="squares square-bottom">
+          <p>17</p>
+        </div>
+        <div className="squares square-bottom">
+          <p>18</p>
+        </div>
+        <div className="squares square-bottom">
+          <p>19</p>
+        </div>
+        <div className="squares square-bottom">
+          <p>20</p>
+          <h4>Quiz</h4>
+        </div>
+        <div className="squares square-bottom">
+          <p>21</p>
+        </div>
+        <div className="squares square-bottom">
+          <p>22</p>
+        </div>
+        <div className="squares square-bottom">
+          <p>23</p>
+        </div>
+        <div className="squares square-bottom">
+          <p>24</p>
+          <h4>Defi</h4>
+        </div>
+        <div className="squares square-bottom">
+          <p>25</p>
+        </div>
+        <div className="squares square-bottom">
+          <p>26</p>
+        </div>
+        <div className="squares square-bottom">
+          <p>27</p>
+        </div>
+        <div className="squares square-bottom">
+          <p>28</p>
+          <h4>Bonne Action</h4>
+        </div>
+      </div>
+
+    </div>
   )
 }
 
