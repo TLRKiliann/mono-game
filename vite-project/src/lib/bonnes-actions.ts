7867a46543b3db91f1a9ab68ef68edf6ba326899
TLRKiliann <philogenie@protonmail.com>
2024-11-24 13:31:22 +0100

    questions updated

---
+++ vite-project/src/lib/bonnes-actions.ts
@@ -0,0 +1,159 @@
+type BonneActionProps = {
+    id: number;
+    title: string;
+    info: string;
+    recompense: string;
+}
+
+export const bonneActionQuestions: BonneActionProps[] = [
+    {
+        id: 1,	
+        title: "Jardin à Papillons",
+        info: "Vous avez planté un jardin favorable aux papillons !",
+        recompense:	"Avancez de deux cases"
+    },
+    {
+        id: 2,	
+        title: "Boost d'Énergie Solaire",
+        info: "Vos panneaux solaires fonctionnent efficacement !",
+        recompense:	"Vous recevez un joker à utiliser en cas de sanctions",
+    },
+    { 
+        id: 3,	
+        title: "Nettoyage de Plage sale",
+        info: "Vous avez organisé un nettoyage réussi de la plage !",
+        recompense:	"Avancez de trois cases."
+    },
+    { 
+        id: 4,	
+        title: "Champion du Recyclage",
+        info: "Vous avez recyclé assidûment cette semaine !",
+        recompense:	"Lancez le dé à nouveau et avancez."
+    },
+    { 
+        id: 5,	
+        title: "Récolte au Jardin Communautaire",
+        info: "Votre jardin communautaire a donné des récoltes abondantes !",
+        recompense:	"Avancez d'une case."
+    },
+    { 
+        id: 6,	
+        title: "Visite à la Réserve Naturelle",
+        info: "Vous avez exploré une réserve naturelle !",
+        recompense:	"Avancez de deux cases"
+    },
+    { 
+        id: 7,	
+        title: "Maison Éconergétique",
+        info: "Votre maison est économe en énergie !",
+        recompense: "Vous recevez un joker à utiliser en cas de sanctions"
+    },
+    { 
+        id: 8,	
+        title: "Journée de Plantation d'Arbres",
+        info: "Vous avez planté des arbres dans le parc local !",
+        recompense: "Avancez de deux cases."
+    },
+    { 
+        id: 9,	
+        title: "Expédition de Nettoyage des Océans",
+        info: "Vous avez participé à un nettoyage des océans !",
+        recompense: "Avancez de trois cases."
+    },
+    { 
+        id: 10,	
+        title: "Déplacement Écologique",
+        info: "Vous avez fait du vélo pour aller travailler cette semaine !",
+        recompense: "Lancez le dé à nouveau et avancez."
+    },
+    { 
+        id: 11,	
+        title: "Succès en Compostage",
+        info: "Votre composteur prospère !",
+        recompense:	"Avancez d'une case."
+    },
+    { 
+        id: 12,	
+        title: "Contrat d'Énergie Verte",
+        info: "Vous avez opté pour l'énergie renouvelable !",
+        recompense:	"Vous recevez un joker à utiliser en cas de sanctions"
+    },
+    { 
+        id: 13,	
+        title: "Aventure d'Observation des Oiseaux",
+        info: "Vous avez repéré des oiseaux rares !",
+        recompense:	"Vous recevez un joker à utiliser en cas de sanctions et avancez deux cases"
+    },
+    { 
+        id: 14,	
+        title: "Pique-Nique Zéro Déchet",
+        info: "Votre pique-nique était sans déchets !",
+        recompense:	"Avancez de deux cases."
+    },
+    { 
+        id: 15,	
+        title: "Concours de Photographie de la Nature",
+        info: "Votre photo de nature a remporté la première place !",
+        recompense:	"Avancez deux cases"
+    },
+    {
+        id: 16,	
+        title: "Boost d'Énergie Renouvelable",
+        info: "Votre maison fonctionne entièrement avec de l'énergie renouvelable !",
+        recompense:	"Avancez de trois cases."
+    },
+    {
+        id: 17,	
+        title: "Journée de Nettoyage Communautaire",
+        info: "Vous avez organisé un nettoyage réussi du quartier !",
+        recompense:	"Vous recevez un joker à utiliser en cas de sanctions et avancez trois cases"
+    },
+    {
+        id: 18,	
+        title: "Innovation Écologique",
+        info: "Votre invention réduit les déchets plastiques !",
+        recompense:	"Avancez de deux cases."
+    },
+    {
+        id: 19,	
+        title: "Marché Fermier Local",
+        info: "Vous avez soutenu les agriculteurs locaux !",
+        recompense:	"Avancez une case"
+    },
+    {
+        id: 20,	
+        title: "Champion du Compostage",
+        info: "Votre composteur prospère !",
+        recompense:	"Célébrez et avancez d'une case."
+    },
+    {
+        id: 21,	
+        title: "Transport Écologique",
+        info: "Vous avez fait du vélo pour aller travailler cette semaine !",
+        recompense:	"Lancez le dé à nouveau et avancez de plusieurs cases."
+    },
+    {
+        id: 22,	
+        title: "Prix de Conservation Océanique",
+        info: "Vos efforts de nettoyage de plage ont été reconnus !",
+        recompense:	"Avancez d'une case."
+    },
+    {
+        id: 23,	
+        title: "Badge d'Éco-Explorateur",
+        info: "Vous avez identifié des espèces sauvages rares !",
+        recompense:	"Avancez deux cases."
+    },
+    {
+        id: 24,	
+        title: "Mode de Vie Zéro Déchet",
+        info: "Votre bocal à déchets est presque vide !",
+        recompense:	"Avancez de deux cases"
+    },
+    {
+        id: 25,	
+        title: "Trophée du Jardinage Écologique",
+        info: "Votre jardin a remporté le premier prix !",
+        recompense:	"Vous recevez un joker à utiliser en cas de sanctions et admirez vos plantes."
+    }
+];
---
+++ vite-project/src/lib/sanctions.ts
@@ -0,0 +1,183 @@
+type SanctionsProps = {
+    id: number;
+    title: string;
+    info: string;
+    consequence: string;
+};
+
+export const sanctionQuestions: SanctionsProps[] = [
+    {
+        id: 1,	
+        title: "Zone d'Incendie de Forêt",
+        info: "Votre campement a pris feu ! Reculez de trois cases.",
+        consequence: "Reculez de trois cases."
+    },
+    {
+        id: 2,
+        title: "Océan de Plastique",
+        info: "Vous avez rencontré une mer de déchets plastiques.",
+        consequence: "Passez votre tour pendant que vous nettoyez"
+    },
+    {
+        id: 3,
+        title: "Désert de Sécheresse",
+        info: "Pénurie d'eau !",
+        consequence: "eculez de deux cases"
+    },
+    {
+        id: 4,
+        title: "Désastre de Déforestation",
+        info: "La forêt dans laquelle vous étiez est maintenant un site de déboisement.",
+        consequence: "eculez de quatre cases"
+    },
+    {
+        id: 5,	
+        title: "Déversement de Pétrole",
+        info: "Votre bateau a heurté une marée noire.",
+        consequence: "Passez votre tour pendant que vous nettoyez le désordre",
+    },
+    {
+        id: 6,	
+        title: "Glacier en Fusion",
+        info: "La glace sous vos pieds fond !",
+        consequence: "Reculez d'une case"
+    },
+    {
+        id: 7,	
+        title: "Habitat d'Espèces en Danger",
+        info: "Vous avez perturbé une zone de nidification.",
+        consequence: "Reculez de deux cases"
+    },
+    {
+        id: 8,	
+        title: "Rivière Polluée",
+        info: "Votre bateau s'est coincé dans une rivière polluée.",												
+        consequence: "Passez votre tour"
+    },
+    {
+        id: 9,	
+        title: "Alerte à la Pollution Atmosphérique",	
+        info: "Alerte au smog !",								
+        consequence: "Reculez d'une case"
+    },
+    {
+        id: 10,
+        title: "Fuite Nucléaire",
+        info: "Zone radioactive !",
+        consequence: "Passez votre tour pendant que vous trouvez un chemin sûr"
+    },
+	{
+        id: 11,	
+        title: "Blanchissement des Coraux",
+        info: "Le récif corallien est en train de mourir.",
+        consequence: "Reculez de trois cases."
+    },
+    {
+        id: 12,
+        title: "Avalanche de Déchets",
+        info: "Une montagne de déchets bloque votre chemin.",
+        consequence: "Reculez de deux cases"
+    },
+    {
+        id: 13,
+        title: "Falaise d'Érosion",
+        info: "Le bord de la falaise s'est effondré.",
+        consequence: "Reculez d'une case"
+    },								
+	{
+        id: 14,	
+        title: "Invasion d'Espèces Envahissantes",
+        info: "Des espèces étrangères ont envahi votre jardin.",
+        consequence: "Retournez à la case départ"
+    },
+    {
+        id: 15,
+        title: "Tempête de Changement Climatique",
+        info: "Conditions météorologiques extrêmes !",
+        consequence: "Retournez à la case départ"
+    },
+    {
+        id: 16,	
+        title: "Décharge de Déchets Électroniques",
+        info: "Vous êtes tombé sur une décharge de déchets électroniques.",
+        consequence: "Passez votre tour pendant que vous vous débarrassez en toute sécurité de vieux gadgets"
+    },	
+	{
+        id: 17,	
+        title: "Alerte Qualité de l'Air",
+        info: "Mauvaise qualité de l'air !",
+        consequence: "Reculez de deux cases"
+    },
+    {
+        id: 18,	
+        title: "Perte de Biodiversité",
+        info: "Destruction de l'habitat !",
+        consequence: "Reculez de trois cases"
+    },
+    {
+        id: 19,	
+        title: "Interdiction des Sacs en Plastique",
+        info: "Vous avez oublié votre sac réutilisable !",
+        consequence: "Reculez d'une case"
+    },
+	{
+        id: 20,	
+        title: "Fuite de Pétrolier",
+        info: "Une fuite de pétrole a bloqué votre chemin.",
+        consequence: "Reculez de quatre cases"
+    },
+    {
+        id: 21,	
+        title: "Habitat des Espèces Menacées",
+        info: "Vous avez perturbé un ours en hibernation.",
+        consequence: "Reculez de deux cases"
+    },
+    {
+        id: 22,	
+        title: "Négation du Changement Climatique",
+        info: "Vous êtes tombé sur un climatosceptique.",
+        consequence: "Passez votre tour pendant que vous les éduquez"
+    },	
+	{
+        id: 23,	
+        title: "Désert Alimentaire",
+        info: "Pas de produits frais à proximité !",
+        consequence: "Reculez d'une case"
+    },
+    {
+        id: 24,	
+        title: "Zone de Pénurie d'Eau",
+        info: "Pénurie d'eau !", 													
+        consequence: "Reculez de deux cases"													
+    },
+    {
+        id: 25,	
+        title: "Mauvaise Manipulation du Recyclage",
+        info: "Vous avez contaminé la poubelle de recyclage.", 													
+        consequence: "Reculez d'une case."
+    },
+	{
+        id: 26,	
+        title: "Site de Déchets Nucléaires",
+        info: "Zone radioactive !",										
+        consequence: "Passez votre tour pendant que vous trouvez un chemin sûr"
+    },
+    {
+        id: 27,	
+        title: "Îlot de Chaleur Urbain",
+        info: "Alerte à la vague de chaleur !",												
+        consequence: "Reculez de trois cases"
+    },
+    {
+        id: 28,	
+        title: "Zone de Surpêche",
+        info: "Épuisement des stocks de poissons !",
+        consequence: "Reculez de deux cases"
+    },
+    {
+        id: 29,	
+        title: "Piège de Greenwashing",
+        info: "Vous êtes tombé dans le piège du faux étiquetage écologique.",
+        consequence: "Retournez à la case départ"
+    }											
+];
