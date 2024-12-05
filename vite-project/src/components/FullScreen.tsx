eff0605facc63d9889c4424009cc3e750dc7dfe5
TLRKiliann <philogenie@protonmail.com>
2024-12-05 16:16:43 +0100

    full screen component created

---
+++ vite-project/src/components/FullScreen.tsx
@@ -0,0 +1,29 @@
+import "./styles/FullScreen.css";
+
+type FullScreenCloseProps = {
+    setCloseFullScreen: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+const FullScreen = ({setCloseFullScreen}: FullScreenCloseProps) => {
+    
+    const handleFullScreen = () => {
+        const element = document.documentElement;
+        if (element.requestFullscreen) {
+            element.requestFullscreen();
+        } else if (element.mozRequestFullScreen) { // Firefox
+            element.mozRequestFullScreen();
+        } else if (element.webkitRequestFullscreen) { // Safari
+            element.webkitRequestFullscreen();
+        } else if (element.msRequestFullscreen) { // IE/Edge
+            element.msRequestFullscreen();
+        }
+        setCloseFullScreen(false);
+    };
+
+    return (
+        <div className="fullscreen-div">
+            <button onClick={handleFullScreen}>Full Screen Mode</button>
+        </div>
+    )
+}
+export default FullScreen;
