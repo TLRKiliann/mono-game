b3c46050f792b4e25d92e82b3124279967d827de
TLRKiliann <philogenie@protonmail.com>
2024-12-06 19:10:25 +0100

    new component created

---
+++ vite-project/src/components/ReadyComponent.tsx
@@ -0,0 +1,55 @@
+import "./styles/ReadyComponent.css";
+
+type SelectOptionProps = {
+    setCloseReady: React.Dispatch<React.SetStateAction<boolean>>; 
+    selectedOption: string;
+};
+
+const ReadyComponent = ({setCloseReady, selectedOption}: SelectOptionProps): JSX.Element => {
+
+    const handleCloseReady = (): void => {
+        setCloseReady(false);
+    };
+
+    return (
+        <div className="div-ready">
+
+            {selectedOption === "français" ? (
+                <div className="div-contentready">
+                    <h1>Joueur "1" c'est ton tour !</h1>
+                    <h2>Lance le dés !!!</h2>
+                </div>
+                ) : null 
+            }
+            {selectedOption === "english" ? (
+                <div className="div-contentready">
+                    <h1>Player "ONE" is your turn !</h1>
+                    <h2>Roll the dice !!!</h2>
+                </div>
+                ) : null 
+            }
+            {selectedOption === "deutsch" ? (
+                <div className="div-contentready">
+                    <h1>Spieler 1, du bist dran !</h1>
+                    <h2>Wirf die Würfel !!!</h2>
+                </div>
+                ) : null 
+            }
+            {selectedOption === "italiano" ? (
+                <div className="div-contentready">
+                    <h1>Giocatore 1, tocca a te !</h1>
+                    <h2>Fai rotolare i dadi !!!</h2>
+                </div>
+                ) : null 
+            }
+
+            <div className="div-btnready">
+                <button type="button" onClick={handleCloseReady}>
+                    Close
+                </button>
+            </div>
+
+        </div>
+    )
+}
+export default ReadyComponent;
