7a851a4f13d9c6005b97d1b6ce66ec309753a874
TLRKiliann <philogenie@protonmail.com>
2024-12-14 11:44:28 +0100

    select a flag for translation done

--- vite-project/src/components/WelcomeComponent.tsx
+++ vite-project/src/components/WelcomeComponent.tsx
@@ -1,63 +1,63 @@
-//import "/node_modules/flag-icons/css/flag-icons.min.css";
+import React from 'react';
+import Select from 'react-select';
+import flagEngland from "../assets/flags/england_icon.png";
+import flagFrance from "../assets/flags/france_icon.png";
+import flagGermany from "../assets/flags/germany_icon.png";
+import flagItaly from "../assets/flags/italy_icon.png";
+
 import "./styles/WelcomeComponent.css";
 
+type OptionsProps = {
+    value: string;
+    label: string;
+    flag: string;
+};
+
 type SelectOptionProps = {
-    selectedOption: string;
     setSelectedOption: React.Dispatch<React.SetStateAction<string>>;
 };
 
-const WelcomeComponent = ({ selectedOption, setSelectedOption }: SelectOptionProps): JSX.Element => {
+const options: OptionsProps[] = [
+    { value: 'français', label: 'Français', flag: flagFrance },
+    { value: 'english', label: 'English', flag: flagEngland },
+    { value: 'deutsch', label: 'Deutsch', flag: flagGermany },
+    { value: 'italiano', label: 'Italiano', flag: flagItaly },
+];
 
-    //const [open, setOpen] = useState<boolean>(false);
+const WelcomeComponent = ({ setSelectedOption }: SelectOptionProps): JSX.Element => {
 
-    const handleChange = (event: React.ChangeEvent<HTMLSelectElement>): void => {
-        setSelectedOption(event.target.value);
+    const handleChange = (option: OptionsProps | null): void => {
+        if (option) {
+            setSelectedOption(option.value);
+        }
     };
-    
-    /* const handleChange = (value: string) => {
-        setSelectedOption(value);
-        setOpen(false);
-    }; */
+
+    const customOption = (props: any): JSX.Element => {
+        const { innerRef, innerProps, data } = props;
+        return (
+            <div ref={innerRef} {...innerProps} className="custom-option">
+                <img src={data.flag} alt={`${data.label} flag`} style={{ width: 20, marginRight: 10 }} />
+                {data.label}
+            </div>
+        );
+    };
 
     return (
         <div className="div-languages">
-
-            <h1>! Bienvenue dans mon ecopot game !</h1>
-
-            {/* <div className="custom-select" onClick={() => setOpen(!open)}>
-                {selectedOption || "-- Choisissez votre langue : --"}
-            </div>
-            {open && (
-                <ul className="options">
-                    <li onClick={() => handleChange("français")}>
-                        <span className="fi fi-fr"></span> Français
-                    </li>
-                    <li onClick={() => handleChange("english")}>
-                        <span className="fi fi-us"></span> English
-                    </li>
-                    <li onClick={() => handleChange("deutsch")}>
-                        <span className="fi fi-de"></span> Deutsch
-                    </li>
-                    <li onClick={() => handleChange("italiano")}>
-                        <span className="fi fi-it"></span> Italiano
-                    </li>
-                </ul>
-            )} */}
-
-            <select id="options" value={selectedOption} onChange={handleChange}>
-                <option value="">-- Choisissez votre langue : --</option>
-                <option value="français">Français</option>
-                <option value="english">English</option>
-                <option value="deutsch">Deutsch</option>
-                <option value="italiano">Italiano</option>
-            </select>
             
-            {selectedOption && <p>{selectedOption === "français" ? "Vous avez choisi: " 
-                : selectedOption === "english" ? "You selected: " 
-                : selectedOption === "deutsch" ? "Sie haben gewählt: " 
-                : "Avete scelto: "} {selectedOption}</p>}
+            <h1>Choisissez votre language :</h1>
+
+            <Select
+                options={options}
+                onChange={handleChange}
+                components={{ Option: customOption }}
+                getOptionValue={(option) => option.value}
+                className="react-select"
+                classNamePrefix="select"
+            />
 
         </div>
-    )
-}
+    );
+};
+
 export default WelcomeComponent;
