ec006126b3b9121898206931b815c8bf0c62d4c6
TLRKiliann <philogenie@protonmail.com>
2024-12-28 21:41:01 +0100

    new order

---
+++ vite-project/src/lib/quiz/quiz_de_lvl1.ts
@@ -0,0 +1,22 @@
+import { QuizProps } from "../types";
+
+export const quizQuestions_de_lvl1: QuizProps[] = [
+    {
+        id: 1,
+        title: "Mobilität",
+        question: "Welches umweltfreundliche Verkehrsmittel benötigt keinen Kraftstoff und trägt zur Verringerung der Luftverschmutzung bei?",
+        response: "Fahrrad"
+    },
+    {
+        id: 2,
+        title: "Essen",
+        question: "Welches Lebensmittel ist eine hervorragende Proteinquelle, wächst unter der Erde und wird oft mit Pommes Frites in Verbindung gebracht?",
+        response: "Kartoffel"
+    },
+    {
+        id: 3,
+        title: "Wasserschutz",
+        question: "Wie kann man auf umweltfreundliche Weise Pflanzen gießen und gleichzeitig den Leitungswasserverbrauch zu Hause reduzieren?",
+        response: "Regenwasser sammeln!"
+    }
+];
