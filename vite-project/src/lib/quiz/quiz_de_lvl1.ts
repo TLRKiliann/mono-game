97bd3bbff59d7ade8c587e06e83ccf134967eb69
TLRKiliann <philogenie@protonmail.com>
2025-01-23 18:44:48 +0100

    adaptation for lvl 1 & 2

--- vite-project/src/lib/quiz/quiz_de_lvl1.ts
+++ vite-project/src/lib/quiz/quiz_de_lvl1.ts
@@ -10,13 +10,415 @@ export const quizQuestions_de_lvl1: QuizProps[] = [
     {
         id: 2,
         title: "Essen",
-        question: "Welches Lebensmittel ist eine hervorragende Proteinquelle, wächst unter der Erde und wird oft mit Pommes Frites in Verbindung gebracht?",
+        question: "Welches Lebensmittel ist eine ausgezeichnete Proteinquelle, wächst unter der Erde und wird oft mit Pommes frites in Verbindung gebracht?",
         response: "Kartoffel"
     },
     {
         id: 3,
         title: "Wasserschutz",
-        question: "Wie kann man auf umweltfreundliche Weise Pflanzen gießen und gleichzeitig den Leitungswasserverbrauch zu Hause reduzieren?",
-        response: "Regenwasser sammeln!"
+        question: "Wie kann man umweltbewusst Pflanzen gießen und den Wasserverbrauch im Haushalt reduzieren?",
+        response: "Regenwassernutzung!"
+    },
+    {
+        id: 4,
+        title: "Klimawandel",
+        question: "Was absorbiert überschüssiges Kohlendioxid aus der Atmosphäre und hilft, die Temperatur der Erde zu regulieren?",
+        response: "Die Wälder!"
+    },
+    {
+        id: 5,
+        title: "Abfallmanagement",
+        question: "Wie nennt man den Prozess, bei dem Abfälle in neue Produkte umgewandelt werden, anstatt sie wegzuwerfen?",
+        response: "Recycling!"
+    },
+    {
+        id: 6,
+        title: "Energieeffizienz",
+        question: "Welche Art von Glühbirne ist energieeffizienter als herkömmliche Glühbirnen?",
+        response: "LED-Lampen"
+    },
+    {
+        id: 7,
+        title: "Wildtierschutz",
+        question: "Welche Meeresbewohner sind bekannt für ihre langen Wanderungen und ihr verspieltes Verhalten?",
+        response: "Delfine"
+    },
+    {
+        id: 8,
+        title: "Gesundheit der Ozeane",
+        question: "Wie nennt man die bunten und vielfältigen Ökosysteme, die unter Wasser, insbesondere vor der Küste Australiens, zu finden sind?",
+        response: "Korallen"
+    },
+    {
+        id: 9,
+        title: "Plastikverschmutzung",
+        question: "Welchen gängigen Plastikartikel sollten wir vermeiden, um Plastikmüll in den Ozeanen zu reduzieren?",
+        response: "Einweg-Plastikstrohhalme"
+    },
+    {
+        id: 10,
+        title: "Erneuerbare Energie",
+        question: "Welche Energiequelle nutzt die Kraft der Sonne zur Stromerzeugung?",
+        response: "Solarmodule"
+    },
+    {
+        id: 11,
+        title: "Mobilität",
+        question: "Welches umweltfreundliche Verkehrsmittel wird von Ihren eigenen Beinen angetrieben und produziert keine Treibhausgase?",
+        response: "Gehen"
+    },
+    {
+        id: 12,
+        title: "Ernährungswahl",
+        question: "Welches Lebensmittel hat eine geringere Umweltbelastung: lokal angebaute Früchte oder exotische Früchte, die von weit her verschifft werden?",
+        response: "Lokal angebaute Früchte"
+    },
+    {
+        id: 13,
+        title: "Wasserschutz",
+        question: "Wie nennt man die Praxis, beim Zähneputzen oder Duschen weniger Wasser zu verwenden?",
+        response: "Wassersparpraktiken"
+    },
+    {
+        id: 14,
+        title: "Klimaheroes",
+        question: "Wer ist die junge Schwedin, die für ihr Klima-Engagement bekannt ist und die weltweiten Klimastreiks inspiriert?",
+        response: "Greta Thunberg"
+    },
+    {
+        id: 15,
+        title: "Abfallreduzierung",
+        question: "Was ist der beste Weg, um Plastikmüll beim Einkaufen zu reduzieren?",
+        response: "Bringen Sie wiederverwendbare Taschen mit"
+    },
+    {
+        id: 16,
+        title: "Erneuerbare Energiequellen",
+        question: "Welche Energiequelle nutzt die Kraft der Gezeiten zur Stromerzeugung?",
+        response: "Gezeitenenergie"
+    },
+    {
+        id: 17,
+        title: "Wildtierschutz",
+        question: "Welches vom Aussterben bedrohte Meerestier ist bekannt für seine spiralförmige Schale?",
+        response: "Nautilus"
+    },
+    {
+        id: 18,
+        title: "Kompostierung",
+        question: "Welcher natürliche Prozess verwandelt Lebensmittelreste und Gartenabfälle in nährstoffreichen Boden?",
+        response: "Kompostierung"
+    },
+    {
+        id: 19,
+        title: "Energieeffiziente Haushaltsgeräte",
+        question: "Welche Art von Glühbirne verbraucht weniger Energie: Glühbirne oder LED?",
+        response: "LED-Lampen"
+    },
+    {
+        id: 20,
+        title: "Biodiversität",
+        question: "Wie nennt man die Vielfalt des Lebens auf der Erde, einschließlich Pflanzen, Tieren und Mikroorganismen?",
+        response: "Die Biodiversität"
+    },
+    {
+        id: 21,
+        title: "Reinigung der Ozeane",
+        question: "Welche Organisation hat das Ziel, Plastikmüll aus den Ozeanen und Flüssen zu entfernen?",
+        response: "The Ocean Cleanup"
+    },
+    {
+        id: 22,
+        title: "Ökologische Materialien",
+        question: "Welches pflanzliche Material ist eine nachhaltige Alternative zu Plastikbesteck?",
+        response: "Bambus"
+    },
+    {
+        id: 23,
+        title: "CO2-Fußabdruck",
+        question: "Wie nennt man die Gesamtmenge an Treibhausgasen, die von einer Person oder einer Aktivität produziert wird?",
+        response: "CO2-Fußabdruck"
+    },
+    {
+        id: 24,
+        title: "Solarenergie",
+        question: "Welche erneuerbare Energiequelle nutzt das Sonnenlicht zur Stromerzeugung?",
+        response: "Solarmodule"
+    },
+    {
+        id: 25,
+        title: "Schutz der Bestäuber",
+        question: "Welche Insekten spielen eine entscheidende Rolle bei der Bestäubung von Blumen und Pflanzen?",
+        response: "Die Bienen"
+    },
+    {
+        id: 26,
+        title: "Grüne Gebäude",
+        question: "Welche Art von Bauweise legt Wert auf Energieeffizienz und nachhaltige Materialien?",
+        response: "Grüne Gebäude"
+    },
+    {
+        id: 27,
+        title: "Recycling von Elektronikabfällen",
+        question: "Wie können wir alte elektronische Geräte wie Smartphones und Laptops verantwortungsbewusst entsorgen?",
+        response: "Indem wir sie in dafür vorgesehenen Zentren recyceln"
+    },
+    {
+        id: 28,
+        title: "Waldschutz",
+        question: "Was ist der Prozess des Pflanzens von Bäumen zur Wiederherstellung von abgeholzten Gebieten?",
+        response: "Aufforstung"
+    },
+    {
+        id: 29,
+        title: "Zero-Waste-Lebensstil",
+        question: "Was ist das Ziel eines Zero-Waste-Lebensstils?",
+        response: "Die Abfallmenge, die auf Deponien gelangt, zu minimieren"
+    },
+    {
+        id: 30,
+        title: "Versauerung der Ozeane",
+        question: "Welcher chemische Prozess macht das Meerwasser saurer und schadet dem Meeresleben?",
+        response: "Die Aufnahme von Kohlendioxid"
+    },
+    {
+        id: 31,
+        title: "Hausgemachte Lebensmittelproduktion",
+        question: "Wie nennt man den Prozess, eigene Gemüse und Kräuter im Garten oder auf dem Balkon anzubauen?",
+        response: "Hausgarten"
+    },
+    {
+        id: 32,
+        title: "Erneuerbare Energie",
+        question: "Welche natürliche Ressource wird von Windkraftanlagen zur Stromerzeugung genutzt?",
+        response: "Der Wind"
+    },
+    {
+        id: 33,
+        title: "Nachhaltige Küche",
+        question: "Was ist eine einfache Möglichkeit, den Energieverbrauch beim Kochen auf dem Herd zu reduzieren?",
+        response: "Verwenden Sie Deckel auf Töpfen und Pfannen"
+    },
+    {
+        id: 34,
+        title: "Nachhaltige Mobilität",
+        question: "Welches umweltfreundliche Verkehrsmittel kombiniert Radfahren und elektrische Energie?",
+        response: "E-Bikes"
+    },
+    {
+        id: 35,
+        title: "Klimaschutzmaßnahmen",
+        question: "Was ist das globale Abkommen zwischen Ländern zur Bekämpfung des Klimawandels?",
+        response: "Das Pariser Abkommen"
+    },
+    {
+        id: 36,
+        title: "Wassersparen zu Hause",
+        question: "Wie können Sie Wasser sparen, wenn Sie das Geschirr von Hand spülen?",
+        response: "Füllen Sie ein Becken, anstatt den Wasserhahn ständig laufen zu lassen"
+    },
+    {
+        id: 37,
+        title: "Energiespartipps für zu Hause",
+        question: "Was ist der einfachste Weg, den Energieverbrauch zu reduzieren, wenn Sie elektronische Geräte nicht verwenden?",
+        response: "Ziehen Sie den Stecker"
+    },
+    {
+        id: 38,
+        title: "Abfallreduktionsstrategien",
+        question: "Was ist das Mantra der drei R, um Abfall zu minimieren?",
+        response: "Reduzieren, Wiederverwenden, Recyceln"
+    },
+    {
+        id: 39,
+        title: "Umweltfreundliches Reisen",
+        question: "Wie können Sie Ihren CO2-Fußabdruck reduzieren, während Sie neue Orte erkunden?",
+        response: "Wählen Sie Züge oder Busse anstelle von Flügen für kürzere Strecken"
+    },
+    {
+        id: 40,
+        title: "Umweltschutz in der Familie",
+        question: "Was ist eine unterhaltsame Aktivität, die Familien gemeinsam unternehmen können, um die Natur zu genießen?",
+        response: "Wandern in der Natur oder ein Picknick machen"
+    },
+    {
+        id: 41,
+        title: "Kompostierung zu Hause",
+        question: "Welche Küchenreste können Sie kompostieren, um nährstoffreichen Boden zu schaffen?",
+        response: "Obstschalen, Kaffeepulver und Eierschalen"
+    },
+    {
+        id: 42,
+        title: "Solar-Gadgets",
+        question: "Welches Außengerät kann Ihr Telefon mit Sonnenlicht aufladen?",
+        response: "Solar-Ladegeräte"
+    },
+    {
+        id: 43,
+        title: "Energieeffiziente Geräte",
+        question: "Was ist der Vorteil der Verwendung von LED-Lampen anstelle von Glühbirnen?",
+        response: "Sie halten länger und verbrauchen weniger Energie"
+    },
+    {
+        id: 44,
+        title: "Reduzierung von Plastikmüll",
+        question: "Wie können Sie Einweg-Plastikflaschen zu Hause vermeiden?",
+        response: "Verwenden Sie eine wiederverwendbare Wasserflasche"
+    },
+    {
+        id: 45,
+        title: "Ökologisches Landscaping",
+        question: "Was ist die nachhaltige Art, Ihren Garten ohne chemische Pestizide zu pflegen?",
+        response: "Indigene Arten pflanzen, die nützliche Insekten anziehen"
+    },
+    {
+        id: 46,
+        title: "Umweltfreundliche Reinigung",
+        question: "Welchen natürlichen Inhaltsstoff können Sie verwenden, um Oberflächen zu reinigen und Flecken zu entfernen?",
+        response: "Weißer Essig"
+    },
+    {
+        id: 47,
+        title: "Verantwortungsbewusster Konsum",
+        question: "Was sollten Sie berücksichtigen, bevor Sie neue Kleidung kaufen, um Modeabfälle zu reduzieren?",
+        response: "Qualität, Vielseitigkeit und Langlebigkeit"
+    },
+    {
+        id: 48,
+        title: "Energiespargewohnheiten",
+        question: "Wie können Sie Ihr Zuhause im Sommer kühl halten, ohne Klimaanlage zu verwenden?",
+        response: "Halten Sie die Jalousien und Vorhänge während der heißesten Stunden geschlossen"
+    },
+    {
+        id: 49,
+        title: "Umweltfreundliche Feierlichkeiten",
+        question: "Was ist eine nachhaltige Alternative zu Einweg-Partydekorationen?",
+        response: "Dekorieren Sie mit wiederverwendbaren Bannern und Stoffen"
+    },
+    {
+        id: 50,
+        title: "Naturwertschätzung",
+        question: "Was ist der beste Weg, um Kindern Umweltmanagement beizubringen?",
+        response: "Indem Sie lokale Parks und Lebensräume erkunden"
+    },
+    {
+        id: 51,
+        title: "Grüner Wasserstoff",
+        question: "Welche saubere Energiequelle wird durch die Spaltung von Wasser mit erneuerbarem Strom erzeugt?",
+        response: "Grüner Wasserstoff"
+    },
+    {
+        id: 52,
+        title: "Geothermie/Lithium-Rückgewinnung",
+        question: "Welche erneuerbare Energiequelle nutzt die Wärme aus dem Inneren der Erde?",
+        response: "Geothermische Energie"
+    },
+    {
+        id: 53,
+        title: "Verkehrsorientierte Technologien",
+        question: "Welche Innovationen verbessern die Energieeffizienz und reduzieren die Emissionen von Fahrzeugen?",
+        response: "Elektrofahrzeuge (EV), Hybridmotoren und öffentliche Verkehrsmittel"
+    },
+    {
+        id: 54,
+        title: "Solar-Gadgets",
+        question: "Welche Arten von Geräten nutzen Sonnenlicht zur Stromerzeugung?",
+        response: "Solar-Ladegeräte, Solarleuchten und tragbare Solarmodule"
+    },
+    {
+        id: 55,
+        title: "Vertikale Landwirtschaft",
+        question: "Welche innovative landwirtschaftliche Technik baut Pflanzen in gestapelten Schichten drinnen an?",
+        response: "Vertikale Landwirtschaft"
+    },
+    {
+        id: 56,
+        title: "Technologien zur Reinigung der Ozeane",
+        question: "Wie können wir Plastikmüll effektiv aus den Ozeanen entfernen?",
+        response: "Durch den Einsatz von schwimmenden Barrieren und autonomen Reinigungsbooten"
+    },
+    {
+        id: 57,
+        title: "Biologisch abbaubare Materialien",
+        question: "Welche umweltfreundlichen Alternativen ersetzen Einwegplastik?",
+        response: "Biologisch abbaubare Kunststoffe aus pflanzlichen Materialien"
+    },
+    {
+        id: 58,
+        title: "Intelligente Thermostate",
+        question: "Welche Geräte optimieren die Heizungs- und Klimatisierungseffizienz in Haushalten?",
+        response: "Intelligente Thermostate"
+    },
+    {
+        id: 59,
+        title: "Satellitenüberwachung der Abholzung",
+        question: "Wie können Satelliten helfen, Wälder und Biodiversität zu schützen?",
+        response: "Indem sie Abholzung und illegale Abholzung aus dem Weltraum überwachen"
+    },
+    {
+        id: 60,
+        title: "Tschernobyl-Katastrophe (1986)",
+        question: "Welches Kernkraftwerk in der Ukraine erlebte 1986 eine katastrophale Kernschmelze, die radioaktive Materialien in die Umwelt freisetzte?",
+        response: "Tschernobyl"
+    },
+    {
+        id: 61,
+        title: "Abholzung im Amazonas-Regenwald",
+        question: "Welche menschliche Aktivität trägt erheblich zur Abholzung des Amazonas-Regenwaldes bei?",
+        response: "Abholzung und Landwirtschaft (wie Viehzucht und Sojaanbau)"
+    },
+    {
+        id: 62,
+        title: "Koalas",
+        question: "Welches ikonische Beuteltier Australiens ist durch Hitzewellen und den Verlust von Lebensräumen aufgrund des Klimawandels bedroht?",
+        response: "Koalas"
+    },
+    {
+        id: 63,
+        title: "Tiere",
+        question: "Welche arktische Art ist mit dem Rückgang des Meereises konfrontiert, was ihre Fähigkeit beeinträchtigt, Robben zu jagen?",
+        response: "Eisbären"
+    },
+    {
+        id: 64,
+        title: "Ozeane",
+        question: "Die steigenden Ozeantemperaturen verursachen das Korallenbleichen. Welche empfindlichen Ökosysteme sind bedroht?",
+        response: "Korallenriffe"
+    },
+    {
+        id: 65,
+        title: "Monarchfalter",
+        question: "Diese wandernden Insekten sind auf bestimmte Pflanzen angewiesen, um sich zu ernähren und zu nisten. Was bedroht ihr Überleben?",
+        response: "Der Klimawandel stört ihre Lebensräume"
+    },
+    {
+        id: 66,
+        title: "Unechte Karettschildkröten",
+        question: "Diese Meeresschildkröten nisten an Stränden. Wie wirkt sich der Anstieg des Meeresspiegels auf ihre Nistplätze aus?",
+        response: "Er überflutet ihre Nester und verringert den Bruterfolg"
+    },
+    {
+        id: 67,
+        title: "Schneeschnecken",
+        question: "In schneebedeckten Regionen ändern diese Schneehühner ihre Fellfarbe. Wie stört der Klimawandel diese Anpassung?",
+        response: "Ihr weißes Fell hebt sich von den braunen Landschaften ab"
+    },
+    {
+        id: 68,
+        title: "Papageitaucher",
+        question: "Diese Meeresvögel sind auf Fischpopulationen angewiesen. Wie beeinflusst die Erwärmung der Ozeane ihre Nahrungsaufnahme?",
+        response: "Sie stört die Verteilung und Verfügbarkeit von Fischen"
+    },
+    {
+        id: 69,
+        title: "Berggorillas",
+        question: "Diese gefährdeten Primaten leben in den tropischen Wäldern Afrikas. Was bedroht ihren Lebensraum?",
+        response: "Die Abholzung aufgrund des Klimawandels"
+    },
+    {
+        id: 70,
+        title: "Harlekinfrösche",
+        question: "Diese bunten Amphibien leben in Nebelwäldern. Wie beeinflusst der Klimawandel ihr Überleben?",
+        response: "Die sich ändernden Temperaturen beeinträchtigen ihre empfindliche Haut und ihren Lebensraum"
     }
 ];
